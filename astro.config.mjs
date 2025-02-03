204dd89c0aec2fbe6c34a975c61362371d31dd94
Camelcompact99 <vazquez.tome.alejandro@gmail.com>
2025-02-03 11:43:15 +0100

    prueba443s

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 // https://astro.build/config
 export default defineConfig({
    
     site: 'https://alejandro-vazquez.com',
     
-    vite: {
-        optimizeDeps: {
-          include: ['three']
-        }
+
+  vite: {
+    optimizeDeps: {
+      include: ["three"]
     },
+  },
 
     i18n:{
         defaultLocale:'es',
---
+++ src/env.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+
+// Add this line to add typings for GSAP
+declare const three: typeof import("three");
+
